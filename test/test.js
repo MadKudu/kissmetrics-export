6e6cc2bd8ca6fb6609fab6fdd05f9f55b11558b6
Paul Cothenet <pcothenet@gmail.com>
2015-04-07 15:14:08 -0700

    Use split as the parser

--- test/test.js
+++ test/test.js
@@ -14,19 +14,24 @@
 		secretAccessKey: process.env.s3_secret_access_key,
 	};
 
-	describe('KmStream', function () {
+	describe('KmProcessor', function () {
 		var parameters = {
 			fromDate: new Date('2015-04-01'),
 			toDate: new Date('2015-04-03')
 		};
-		var KmProcessor = require('../lib/index');
-		var kmProcessor = new KmProcessor(config);
-		var stream = kmProcessor.stream(parameters);
-		describe('bla', function() {
+		var KmExport = require('../lib/index');
+		var kmExport = new KmExport(config);
+		describe('parsed_stream', function() {
+			var stream = kmExport.stream(parameters);
 			it('should stream data', function (done) {
 				this.timeout('60000');
 				stream.on('data', function(data) {
+					console.log(data);
 					expect(data).to.be.not.null;
+				});
+				stream.on('error', function(err) {
+					console.log(err.stack);
+					expect(0).to.equal(1);
 				});
 				stream.on('end', function() {
 					console.log('done');
